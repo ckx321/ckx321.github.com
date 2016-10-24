03359851c16e3de0ac7517a937896de42838e6b8
ckx321 <303813367@qq.com>
2016-10-24 14:54:39 +0800

    xiugai

--- wordpress/wp-content/themes/twentyfourteen/footer.php
+++ wordpress/wp-content/themes/twentyfourteen/footer.php
@@ -9,7 +9,14 @@
  * @since Twenty Fourteen 1.0
  */
 ?>
-
+<div id="SOHUCS" sid="" ></div> 
+<script type="text/javascript"> 
+(function(){ 
+var appid = 'cysF6HfdE'; 
+var conf = 'prod_d70f41abcc778e210bdbd71516bf8007'; 
+var width = window.innerWidth || document.documentElement.clientWidth; 
+if (width < 960) { 
+window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); } })(); </script>
 		</div><!-- #main -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
